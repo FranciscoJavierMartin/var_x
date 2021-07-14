94848e35bda5bb97fcb904c9efb38d38fc1f5a65
Francisco <javiermartincuenca@gmail.com>
2021-07-14 23:18:11 +0200

    Add backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '49ace41414218cfe0574dea0ad908e58'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5b7ce754-17fa-4e0f-be22-695f9ec81703'
+};
