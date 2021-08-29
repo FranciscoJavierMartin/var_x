34e45914ac61740fe858655c931e1a9b46a0fdf5
Francisco <javiermartincuenca@gmail.com>
2021-08-29 11:28:05 +0200

    Add cart actions

---
+++ frontend/src/contexts/cart/actions.ts
@@ -0,0 +1,54 @@
+import { Variant } from '../../interfaces/product-details';
+
+export enum CartActionTypes {
+  ADD_TO_CART = 'ADD_TO_CART',
+  REMOVE_FROM_CART = 'REMOVE_FROM_CART',
+  CLEAR_CART = 'CLEAR_CART',
+}
+
+export interface AddToCartType {
+  type: CartActionTypes.ADD_TO_CART;
+  payload: {
+    variant: Variant;
+    qty: number;
+    name: string;
+  };
+}
+
+export interface RemoveFromCartType {
+  type: CartActionTypes.REMOVE_FROM_CART;
+  payload: {
+    variant: Variant;
+    qty: number;
+  };
+}
+
+export interface ClearCartType {
+  type: CartActionTypes.CLEAR_CART;
+}
+
+export type CartActionsTypes =
+  | AddToCartType
+  | RemoveFromCartType
+  | ClearCartType;
+
+export const addToCart = (
+  variant: Variant,
+  qty: number,
+  name: string
+): AddToCartType => ({
+  type: CartActionTypes.ADD_TO_CART,
+  payload: { variant, qty, name },
+});
+
+export const removeFromCart = (
+  variant: Variant,
+  qty: number
+): RemoveFromCartType => ({
+  type: CartActionTypes.REMOVE_FROM_CART,
+  payload: { variant, qty },
+});
+
+export const clearCart = (): ClearCartType => ({
+  type: CartActionTypes.CLEAR_CART,
+});
