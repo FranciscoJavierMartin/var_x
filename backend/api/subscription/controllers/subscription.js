439a6d103c61cf02bddbe980adfe37d229494fd0
Francisco <javiermartincuenca@gmail.com>
2021-10-31 21:55:43 +0100

    get subscriptions backend

--- backend/api/subscription/controllers/subscription.js
+++ backend/api/subscription/controllers/subscription.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async userSubscriptions(ctx) {
+    const subscriptions = await strapi.services.subscription
+      .find({
+        user: ctx.state.user.id,
+      })
+      .map((subscription) => {
+        delete subscription.user;
+        return sanitizeEntity(subscription, {
+          model: strapi.models.subscription,
+        });
+      });
+
+    ctx.send(subscriptions, 200);
+  },
+};
